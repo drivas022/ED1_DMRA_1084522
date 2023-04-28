19d2291e81e3a46d41108fac4774bc5074f0f77e
di3gorivas <DMRIVASA@correo.url.edu.gt>
2023-04-27 22:41:58 -0600

    creando las clases LAB02

---
+++ LAB02_ED1_DMRA/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LAB02_ED1_DMRA
+{
+    internal class Program
+    {
+        public class InputLab
+        {
+            public Input1[] input1 { get; set; }
+            public Input2 input2 { get; set; }
+        }
+        public class Input1
+        {
+            public Dictionary<string, bool> services { get; set; }
+            public Builds builds { get; set; }
+        }
+
+        public class Input2
+        {
+            public double budget { get; set; }
+            public string typeBuilder { get; set; }
+            public string[] requiredServices { get; set; }
+            public string? commercialActivity { get; set; }
+            public bool? wannaPetFriendly { get; set; }
+            public string? minDanger { get; set; }
+        }
+        public class Houses
+        {
+            public string zoneDangerous { get; set; }
+            public string address { get; set; }
+            public double price { get; set; }
+            public string contactPhone { get; set; }
+            public string id { get; set; }
+        }
+
+        public class Apartments
+        {
+            public bool isPetFriendly { get; set; }
+            public string address { get; set; }
+            public double price { get; set; }
+            public string contactPhone { get; set; }
+            public string id { get; set; }
+        }
+
+        public class Premises
+        {
+            public string[] commercialActivities { get; set; }
+            public string address { get; set; }
+            public double price { get; set; }
+            public string contactPhone { get; set; }
+            public string id { get; set; }
+        }
+
+        public class Builds
+        {
+            public Premises[]? Premises { get; set; }
+            public Apartments[]? Apartments { get; set; }
+            public Houses[]? Houses { get; set; }
+        }
+                
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
